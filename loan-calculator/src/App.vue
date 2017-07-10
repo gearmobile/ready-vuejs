91a59ae00f64941c9cd25f179efe124d3deac3ff
gearmobile <gearmobile@gmail.com>
2017-07-10 13:24:29 +0300

    loan

--- loan-calculator/src/App.vue
+++ loan-calculator/src/App.vue
@@ -25,14 +25,17 @@
 
         main.elevation-6
           v-layout( row, wrap )
+            // left column
             v-flex.text-xs-right.blue.darken-1.white--text.loan-calc--vert.hidden-sm-and-down( xs12, md4 )
               h4.white--text.mb-0.pr-2
                 | автоломбард
               p.mb-0.pr-2
                 | машина остается на нашей стоянке
+            // center column
             v-flex.red.darken-1.py-5.loan-calc--center( xs12, md4 )
               h4.text-xs-center.white--text.mb-0
                 | программа
+            // right column
             v-flex.text-xs-left.green.darken-1.loan-calc--vert.hidden-sm-and-down( xs12, md4 )
               h4.white--text.mb-0.pl-2
                 | автозалог
@@ -41,35 +44,50 @@
                 br
                 | автомобиль остается у вас
 
+          // CALCULATION
+
           v-layout( row )
+            // left column
             v-flex.text-xs-right.blue.darken-2.white--text.loan-calc--right( xs4 )
               h6.mb-0.white--text.pr-2
                 | {{ sum1 | currency }}
+            // center column
             v-flex.red.darken-2.loan-calc--center( xs4 )
-              h5.text-xs-center.white--text.mb-0.py-3
+              h6.text-xs-center.white--text.mb-0.py-3
                 | сумма займа
+            // right column
             v-flex.text-xs-left.green.darken-2.loan-calc--left( xs4 )
               h6.mb-0.white--text.pl-2
                 | {{ sum2 | currency }}
 
+          // CALCULATION
+
           v-layout( row )
+            // left column
             v-flex.text-xs-right.blue.darken-3.white--text.loan-calc--right( xs4 )
               h6.mb-0.white--text.pr-2
                 | {{ pawnshop.percent | percent }}
+            // center column
             v-flex.red.darken-3.loan-calc--center( xs4 )
-              h5.text-xs-center.white--text.mb-0.py-3
+              h6.text-xs-center.white--text.mb-0.py-3
                 | процентная ставка
+            // right column
             v-flex.text-xs-left.green.darken-3.loan-calc--left( xs4 )
               h6.mb-0.white--text.pl-2
                 | {{ zalog.percent | percent }}
 
+          // CALCULATION
+
           v-layout( row )
+            // left column
             v-flex.text-xs-right.blue.darken-4.white--text.loan-calc--right( xs4 )
               h6.mb-0.white--text.pr-2
                 | {{ payment1 | currency }}
+            // center column
             v-flex.red.darken-4.loan-calc--center( xs4 )
-              h5.text-xs-center.white--text.mb-0.py-3
+              h6.text-xs-center.white--text.mb-0.py-3
                 | ежемесячный платеж
+            // right column
             v-flex.text-xs-left.green.darken-4.loan-calc--left( xs4 )
               h6.mb-0.white--text.pl-2
                 | {{ payment2 | currency }}
