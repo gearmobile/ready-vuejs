680be39e241c836d4aca748db06cf7ac3a1206a7
gearmobile <gearmobile@gmail.com>
2017-12-10 12:27:21 +0300

    meetup - change image dimentions by upload

--- meetup-app/src/components/Meetup/MeetupNew.vue
+++ meetup-app/src/components/Meetup/MeetupNew.vue
@@ -54,7 +54,7 @@
             img(
               :src="meetup.src",
               :alt="meetup.title",
-              style="width: 100%; height: auto"
+              style="width: 100%; height: 300px;"
             )
 
           // DESCRIPTION SECTION
